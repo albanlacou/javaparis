254a4f78574c2629291c3e307d461c299720061c
Vincent <vleclerc@inow.fr>
2021-11-15 15:05:22 +0100

    add user model

---
+++ javaav/src/main/java/com/coding/javaav/models/User.java
@@ -0,0 +1,49 @@
+package com.coding.javaav.models;
+
+public class User {
+    private Long id;
+    private String lastname;
+    private String firstname;
+    private String email;
+    private String phone;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+}
