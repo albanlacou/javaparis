ac27cd949de6797c37ac8df8a513332730c9a96e
Vincent <vleclerc@inow.fr>
2021-11-15 14:33:49 +0100

    add controller and view

---
+++ javaav/src/main/java/com/coding/javaav/UsersController.java
@@ -0,0 +1,13 @@
+package com.coding.javaav;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class UsersController {
+
+    @RequestMapping("/users")
+    public String index(){
+        return "index";
+    }
+}
